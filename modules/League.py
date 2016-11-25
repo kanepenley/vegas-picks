f9934f898a7293387cefc1358dc0592d49041cbf
kanepenley <kanepenley@gmail.com>
2016-11-25 16:26:12 -0500

    Constants file to plug in API key, will be added to .gitignore soon

--- modules/League.py
+++ modules/League.py
@@ -1,7 +1,14 @@
+import Player
+
 class League():
     """Object to represent a league."""
     def __init__(self, players):
         self.players = players
+
+    def addPlayer(name):
+        player = Player.Player(name)
+        self.players.append(player)
+
     def printPlayers(self):
         for p in self.players:
             print p.name
